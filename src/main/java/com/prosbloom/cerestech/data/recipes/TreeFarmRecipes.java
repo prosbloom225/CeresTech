faa60de6a3425eea0d3a25e2b7545bdbbcd3df8f
prosbloom225 <prosbloom@gmail.com>
2023-10-09 12:44:08 -0500

    adding rest of vanilla saplings

--- src/main/java/com/prosbloom/cerestech/data/recipes/TreeFarmRecipes.java
+++ src/main/java/com/prosbloom/cerestech/data/recipes/TreeFarmRecipes.java
@@ -1,5 +1,7 @@
 package com.prosbloom.cerestech.data.recipes;
 
+import com.gregtechceu.gtceu.common.data.GTBlocks;
+import com.gregtechceu.gtceu.common.data.GTItems;
 import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.ItemStack;
@@ -13,10 +15,41 @@ import static com.prosbloom.cerestech.data.CTRecipeTypes.TREE_FARM_RECIPES;
 
 public class TreeFarmRecipes {
     public static void registerTreeFarmRecipes(Consumer<FinishedRecipe> provider) {
-        TREE_FARM_RECIPES.recipeBuilder("sapling_to_logs")
+        TREE_FARM_RECIPES.recipeBuilder("farm_oak")
                 .inputItems(Items.OAK_SAPLING, 1)
-                .inputFluids(Water.getFluid(1000))
+                .inputFluids(Water.getFluid(100))
                 .outputItems(new ItemStack(Items.OAK_LOG, 4), new ItemStack(Items.OAK_SAPLING, 1))
+                .chancedOutput(new ItemStack(Items.APPLE, 1), 5000, 1000)
+                .duration(100).EUt(96)
+                .save(provider);
+        TREE_FARM_RECIPES.recipeBuilder("farm_spruce")
+                .inputItems(Items.SPRUCE_SAPLING, 1)
+                .inputFluids(Water.getFluid(100))
+                .outputItems(new ItemStack(Items.SPRUCE_LOG, 4), new ItemStack(Items.SPRUCE_SAPLING, 1))
+                .duration(100).EUt(96)
+                .save(provider);
+        TREE_FARM_RECIPES.recipeBuilder("farm_acacia")
+                .inputItems(Items.ACACIA_SAPLING, 1)
+                .inputFluids(Water.getFluid(100))
+                .outputItems(new ItemStack(Items.ACACIA_LOG, 4), new ItemStack(Items.ACACIA_SAPLING, 1))
+                .duration(100).EUt(96)
+                .save(provider);
+        TREE_FARM_RECIPES.recipeBuilder("farm_jungle")
+                .inputItems(Items.JUNGLE_SAPLING, 1)
+                .inputFluids(Water.getFluid(100))
+                .outputItems(new ItemStack(Items.JUNGLE_LOG, 4), new ItemStack(Items.JUNGLE_SAPLING, 1))
+                .duration(100).EUt(96)
+                .save(provider);
+        TREE_FARM_RECIPES.recipeBuilder("farm_birch")
+                .inputItems(Items.BIRCH_SAPLING, 1)
+                .inputFluids(Water.getFluid(100))
+                .outputItems(new ItemStack(Items.BIRCH_LOG, 4), new ItemStack(Items.BIRCH_SAPLING, 1))
+                .duration(100).EUt(96)
+                .save(provider);
+        TREE_FARM_RECIPES.recipeBuilder("farm_dark_oak")
+                .inputItems(Items.DARK_OAK_SAPLING, 1)
+                .inputFluids(Water.getFluid(100))
+                .outputItems(new ItemStack(Items.DARK_OAK_LOG, 4), new ItemStack(Items.DARK_OAK_SAPLING, 1))
                 .duration(100).EUt(96)
                 .save(provider);
     }

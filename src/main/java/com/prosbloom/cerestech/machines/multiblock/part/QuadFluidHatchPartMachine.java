8bd16a72ad6b9f006d4175bccf0028e3019b27a7
prosbloom225 <prosbloom@gmail.com>
2023-10-10 21:33:10 -0500

    fix quad hatches

--- src/main/java/com/prosbloom/cerestech/machines/multiblock/part/QuadFluidHatchPartMachine.java
+++ src/main/java/com/prosbloom/cerestech/machines/multiblock/part/QuadFluidHatchPartMachine.java
@@ -3,18 +3,26 @@ package com.prosbloom.cerestech.machines.multiblock.part;
 import com.gregtechceu.gtceu.api.capability.recipe.IO;
 import com.gregtechceu.gtceu.api.gui.GuiTextures;
 import com.gregtechceu.gtceu.api.machine.IMachineBlockEntity;
+import com.gregtechceu.gtceu.api.machine.multiblock.part.TieredIOPartMachine;
 import com.gregtechceu.gtceu.api.machine.trait.NotifiableFluidTank;
 import com.gregtechceu.gtceu.common.machine.multiblock.part.FluidHatchPartMachine;
 import com.lowdragmc.lowdraglib.gui.widget.ImageWidget;
 import com.lowdragmc.lowdraglib.gui.widget.TankWidget;
 import com.lowdragmc.lowdraglib.gui.widget.Widget;
 import com.lowdragmc.lowdraglib.gui.widget.WidgetGroup;
+import com.lowdragmc.lowdraglib.side.fluid.FluidTransferHelper;
+import com.lowdragmc.lowdraglib.syncdata.annotation.Persisted;
+import com.lowdragmc.lowdraglib.syncdata.field.ManagedFieldHolder;
 import com.prosbloom.cerestech.api.machine.trait.NotifiableFluidTankMulti;
+import net.minecraft.server.TickTask;
+import net.minecraft.server.level.ServerLevel;
 
 public class QuadFluidHatchPartMachine extends FluidHatchPartMachine  {
+    protected static final ManagedFieldHolder MANAGED_FIELD_HOLDER = new ManagedFieldHolder(QuadFluidHatchPartMachine.class, TieredIOPartMachine.MANAGED_FIELD_HOLDER);
     // TODO - autoIO
     // TODO - New Texture
     private final int numTanks = 4;
+    @Persisted
     private NotifiableFluidTankMulti tanks;
 
     public QuadFluidHatchPartMachine(IMachineBlockEntity holder, int tier, IO io, Object... args) {
@@ -22,10 +30,49 @@ public class QuadFluidHatchPartMachine extends FluidHatchPartMachine  {
     }
 
     @Override
+    public ManagedFieldHolder getFieldHolder() {
+        return MANAGED_FIELD_HOLDER;
+    }
+
+    @Override
     protected NotifiableFluidTank createTank(Object... args) {
         tanks = new NotifiableFluidTankMulti(this, numTanks, getTankCapacity(), io);
         // dummy tank
         return new NotifiableFluidTank(this, 0, getTankCapacity(), io);
+    }
+
+    @Override
+    public void updateTankSubscription() {
+        if (isWorkingEnabled() && ((io == IO.OUT && !tanks.isEmpty()) || io == IO.IN)
+                && FluidTransferHelper.getFluidTransfer(getLevel(), getPos().relative(getFrontFacing()), getFrontFacing().getOpposite()) != null) {
+            autoIOSubs = subscribeServerTick(autoIOSubs, this::autoIO);
+        } else if (autoIOSubs != null) {
+            autoIOSubs.unsubscribe();
+            autoIOSubs = null;
+        }
+    }
+
+    @Override
+    protected void autoIO() {
+        if (getOffsetTimer() % 5 == 0) {
+            if (isWorkingEnabled()) {
+                if (io == IO.OUT) {
+                    tanks.exportToNearby(getFrontFacing());
+                } else if (io == IO.IN){
+                    tanks.importFromNearby(getFrontFacing());
+                }
+            }
+            updateTankSubscription();
+        }
+    }
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+        if (getLevel() instanceof ServerLevel serverLevel) {
+            serverLevel.getServer().tell(new TickTask(0, this::updateTankSubscription));
+        }
+        tankSubs = tanks.addChangedListener(this::updateTankSubscription);
     }
 
     @Override

bc80568421c88ff477e963a927bbd997400677b4
prosbloom225 <prosbloom@gmail.com>
2023-10-29 10:01:00 -0500

    updates to me stocking bus

--- src/main/java/com/prosbloom/cerestech/machines/multiblock/part/MEInputBusPartMachine.java
+++ src/main/java/com/prosbloom/cerestech/machines/multiblock/part/MEInputBusPartMachine.java
@@ -8,6 +8,8 @@ import com.gregtechceu.gtceu.api.gui.GuiTextures;
 import com.gregtechceu.gtceu.api.machine.IMachineBlockEntity;
 import com.gregtechceu.gtceu.api.machine.feature.multiblock.IDistinctPart;
 import com.gregtechceu.gtceu.api.machine.multiblock.part.TieredIOPartMachine;
+import com.gregtechceu.gtceu.api.machine.trait.IRecipeHandlerTrait;
+import com.gregtechceu.gtceu.api.machine.trait.MachineTrait;
 import com.gregtechceu.gtceu.api.machine.trait.NotifiableItemStackHandler;
 import com.lowdragmc.lowdraglib.gui.widget.SlotWidget;
 import com.lowdragmc.lowdraglib.gui.widget.Widget;
@@ -16,12 +18,29 @@ import com.lowdragmc.lowdraglib.syncdata.annotation.Persisted;
 import com.lowdragmc.lowdraglib.syncdata.field.ManagedFieldHolder;
 import net.minecraft.world.item.ItemStack;
 
+import java.util.List;
+
 public class MEInputBusPartMachine extends MEItemPartMachine implements IDistinctPart {
 
     protected static final ManagedFieldHolder MANAGED_FIELD_HOLDER = new ManagedFieldHolder(MEInputBusPartMachine.class, MEItemPartMachine.MANAGED_FIELD_HOLDER);
 
     @Persisted
     public final NotifiableItemStackHandler meInventory;
+
+    @Override
+    public List<MachineTrait> getTraits() {
+        return super.getTraits();
+    }
+
+    @Override
+    public List<IRecipeHandlerTrait> getRecipeHandlers() {
+        // ignore meInventory for recipes
+        // TODO - doesnt work at all
+        return traits.stream().filter(IRecipeHandlerTrait.class::isInstance)
+                .filter(t->t.equals(inventory))
+                .map(IRecipeHandlerTrait.class::cast).toList();
+    }
+
     public MEInputBusPartMachine(IMachineBlockEntity holder, int tier, IO io) {
         super(holder, tier, io);
         meInventory = new NotifiableItemStackHandler(this, getInventorySize(), io);
@@ -39,7 +58,6 @@ public class MEInputBusPartMachine extends MEItemPartMachine implements IDistinc
                     return;
                 var storage = grid.getStorageService().getInventory();
 
-                // TODO - still not working quite right
                 for (int j = 0; j < inventory.getSlots(); j++) {
                     ItemStack is = inventory.storage.getStackInSlot(j).copy();
                     if (is.getItem() != meInventory.storage.getStackInSlot(j).getItem() && !meInventory.storage.getStackInSlot(j).isEmpty()) {
@@ -73,13 +91,18 @@ public class MEInputBusPartMachine extends MEItemPartMachine implements IDistinc
             rowSize = 4;
             colSize = 2;
         }
-        var group = new WidgetGroup(0, 0, 18 * rowSize + 16, 18 * colSize + 16);
-        var container = new WidgetGroup(4, 4, 18 * rowSize + 8, 18 * colSize + 8);
+        var group = new WidgetGroup(0, 0, 18 * rowSize*2 + 20, 18 * colSize + 16);
+        var container = new WidgetGroup(4, 4, 18 * rowSize*2 + 12, 18 * colSize + 8);
         int index = 0;
         for (int y = 0; y < colSize; y++) {
             for (int x = 0; x < rowSize; x++) {
-                container.addWidget(new SlotWidget(inventory.storage, index++, 4 + x * 18, 4 + y * 18, true, true)
+                // inv
+                container.addWidget(new SlotWidget(inventory.storage, index, 4 + x * 18, 4 + y * 18, true, true)
                         .setBackgroundTexture(GuiTextures.SLOT));
+                // me mirror
+                container.addWidget(new SlotWidget(meInventory.storage, index, (rowSize * 18) + 8 + x * 18, 4 + y * 18, false, false)
+                        .setBackgroundTexture(GuiTextures.SLOT_DARKENED));
+                index++;
             }
         }
 

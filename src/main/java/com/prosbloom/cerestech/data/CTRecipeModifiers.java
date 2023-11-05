cb11464a6ecce8486f2e04b7a718d878411e0d58
prosbloom225 <prosbloom@gmail.com>
2023-11-05 09:23:58 -0600

    fix overclock for volcanus/cryo

--- src/main/java/com/prosbloom/cerestech/data/CTRecipeModifiers.java
+++ src/main/java/com/prosbloom/cerestech/data/CTRecipeModifiers.java
@@ -4,12 +4,14 @@ import com.gregtechceu.gtceu.api.GTValues;
 import com.gregtechceu.gtceu.api.capability.recipe.IRecipeCapabilityHolder;
 import com.gregtechceu.gtceu.api.capability.recipe.RecipeCapability;
 import com.gregtechceu.gtceu.api.machine.MetaMachine;
+import com.gregtechceu.gtceu.api.machine.feature.IOverclockMachine;
 import com.gregtechceu.gtceu.api.machine.multiblock.CoilWorkableElectricMultiblockMachine;
 import com.gregtechceu.gtceu.api.recipe.GTRecipe;
 import com.gregtechceu.gtceu.api.recipe.OverclockingLogic;
 import com.gregtechceu.gtceu.api.recipe.RecipeHelper;
 import com.gregtechceu.gtceu.api.recipe.content.Content;
 import com.gregtechceu.gtceu.api.recipe.content.ContentModifier;
+import com.gregtechceu.gtceu.common.data.GTRecipeModifiers;
 
 import javax.annotation.Nonnull;
 import java.util.List;
@@ -18,38 +20,44 @@ import java.util.Map;
 public class CTRecipeModifiers {
     public static GTRecipe volcanusParallel(MetaMachine machine, @Nonnull GTRecipe recipe, int maxParallel, boolean modifyDuration) {
         recipe = ebfOverclock(machine, recipe);
-        // TODO - clean this up, shouldnt have to override these to nest.. Also need to set programmable_circuit stacksize to 1 on parallelized recipes
         if (machine instanceof IRecipeCapabilityHolder holder) {
-            return tryParallel(holder, recipe, 1, maxParallel, modifyDuration);
+            recipe = tryParallel(holder, recipe, 1, maxParallel, modifyDuration, false);
+            recipe = RecipeHelper.applyOverclock(OverclockingLogic.PERFECT_OVERCLOCK, recipe,
+                    ((IOverclockMachine)machine).getOverclockVoltage());
+            return recipe;
         }
         return null;
     }
     public static GTRecipe cryogenicParallel(MetaMachine machine, @Nonnull GTRecipe recipe, int maxParallel, boolean modifyDuration) {
-        // TODO - clean this up, shouldnt have to override these to nest.. Also need to set programmable_circuit stacksize to 1 on parallelized recipes
         if (machine instanceof IRecipeCapabilityHolder holder) {
-            return tryParallel(holder, recipe, 1, maxParallel, modifyDuration);
+            recipe = tryParallel(holder, recipe, 1, maxParallel, modifyDuration, true);
+            if (recipe != null)
+                return RecipeHelper.applyOverclock(OverclockingLogic.PERFECT_OVERCLOCK, recipe, ((IOverclockMachine)machine).getOverclockVoltage());
         }
         return null;
     }
 
-    private static GTRecipe tryParallel(IRecipeCapabilityHolder holder, GTRecipe original, int min, int max, boolean modifyDuration) {
+    private static GTRecipe tryParallel(IRecipeCapabilityHolder holder, GTRecipe original, int min, int max, boolean modifyDuration, boolean modifyVoltage) {
         if (min > max) return null;
         int mid = (min + max) / 2;
         GTRecipe copied = original.copy(ContentModifier.multiplier(mid), modifyDuration);
         // set euT back to original
-        Map.Entry<RecipeCapability<?>, List<Content>> newEu =  copied.tickInputs.entrySet().iterator().next();
-        Map.Entry<RecipeCapability<?>, List<Content>> originalEu =  original.tickInputs.entrySet().iterator().next();
-        copied.tickInputs.put(newEu.getKey(), originalEu.getValue());
+        if (!modifyVoltage) {
+            Map.Entry<RecipeCapability<?>, List<Content>> newEu = copied.tickInputs.entrySet().iterator().next();
+            Map.Entry<RecipeCapability<?>, List<Content>> originalEu = original.tickInputs.entrySet().iterator().next();
+            copied.tickInputs.put(newEu.getKey(), originalEu.getValue());
+        }
+        // set duration properly
         if (!copied.matchRecipe(holder).isSuccess() || !copied.matchTickRecipe(holder).isSuccess()) {
             // tried too many
-            return tryParallel(holder, original, min, mid - 1, modifyDuration);
+            return tryParallel(holder, original, min, mid - 1, modifyDuration, modifyVoltage);
         } else {
             // at max parallels
             if (mid == max) {
                 return copied;
             }
             // matches, but try to do more
-            var tryMore = tryParallel(holder, original, mid + 1, max, modifyDuration);
+            var tryMore = tryParallel(holder, original, mid + 1, max, modifyDuration, modifyVoltage);
             return tryMore != null ? tryMore : copied;
         }
     }

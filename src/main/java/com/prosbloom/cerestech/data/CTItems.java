567a3f67bfbb2f48a79e279757a3d805b6970cab
prosbloom225 <prosbloom@gmail.com>
2023-12-05 10:27:07 -0600

    fixing datagen.. using datagen for item models

--- src/main/java/com/prosbloom/cerestech/data/CTItems.java
+++ src/main/java/com/prosbloom/cerestech/data/CTItems.java
@@ -25,6 +25,8 @@ public class CTItems {
     public static ItemEntry<Item> WASTE_METAL_GROUP_C = REGISTRATE.item("waste_metal_group_c", Item::new).lang("Metal Group C Waste").register();
     public static ItemEntry<Item> WASTE_NONMETAL = REGISTRATE.item("waste_nonmetal", Item::new).lang("Nonmetal Waste").register();
     public static ItemEntry<Item> WASTE_METALOID = REGISTRATE.item("waste_metaloid", Item::new).lang("Metaloid Waste").register();
+
+    // TODO - uhv motor/emitter missing from gtceu...remove these when upstream implements
     public static ItemEntry<Item> ELECTRIC_MOTOR_UHV = REGISTRATE.item("uhv_electric_motor", Item::new).lang("UHV Electric Motor").register();
     public static ItemEntry<Item> EMITTER_UHV = REGISTRATE.item("uhv_emitter", Item::new).lang("UHV Emitter").register();
 
